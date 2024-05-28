659ba4f452c82b156769b622a75509c0564e4bff
Chenhao <xch927027@pku.edu.cn>
2024-05-28 15:11:35 +0800

    env.sh

---
+++ env.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Before running this script, make sure you have setup a conda environment with the following command:
+# ````
+# conda create -n easymac
+# conda install openjdk=8
+# conda install -c conda-forge sbt
+# conda install -c conda-forge verilator
+# ````
+
+conda activate easymac
